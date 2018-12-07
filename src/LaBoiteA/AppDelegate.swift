83b8a492e96e6442a90f201b747c22a08a2ef7d2
Pier-Lionel Sgard <pl.sgard@diple.io>
2018-12-07 15:12:00 +0100

    Status bar

--- src/LaBoiteA/AppDelegate.swift
+++ src/LaBoiteA/AppDelegate.swift
@@ -11,19 +11,35 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         if #available(OSX 10.12.1, *) {
             NSApplication.shared.isAutomaticCustomizeTouchBarMenuItemEnabled = true
         }
+        
+        if let button = statusItem.button {
+            let icon = NSImage(named:NSImage.Name("Logo"))
+            icon?.isTemplate = true
+            button.image = icon
+            //button.action = #selector(printQuote(_:))
+        }
+        
+        constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Quit LBA", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
 
 }
 
